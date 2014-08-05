3e9d7397f217550e24909c1c843642f8c04a9151
Graulf <raul.iribarren.c@gmail.com>
2014-08-05 11:30:26 +0200

    admin/index.php
    Fixat med admin index, hantering av inlägg

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,90 @@
 <?php
-// inkluderar config-filen 
+// Inkluderar config-filen 
 require_once('../includes/config.php');
 
-// kollar om användaren är inloggad, annars skickas man till loggain.php
+// Kollar om användaren är inloggad, annars skickas man till loggain.php
 if(!$user -> is_logged_in()) { header('Location: loggain.php'); }
+
+if(isset($_GET['delpost'])){ 
+
+	$stmt = $db->prepare('DELETE FROM blog_posts WHERE postID = :postID') ;
+	$stmt->execute(array(':postID' => $_GET['delpost']));
+
+	header('Location: index.php?action=deleted');
+	exit;
+} 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Admin</title>
+  <link rel="stylesheet" href="../style/normalize.css">
+  <link rel="stylesheet" href="../style/main.css">
+  <script language="JavaScript" type="text/javascript">
+  function delpost(id, title)
+  {
+  	if (confirm("Är du säker på att dy vill radera '" + title + "'"))
+	  {
+	  	window.location.href = 'index.php?delpost=' + id;
+	  }
+  }
+  </script>
+</head>
+<body>
+
+	<div id="wrapper">
+
+	<?php include('meny.php');?>
+
+	<?php 
+	// Visa meddelande från skriv-post/redigera-post
+	if(isset($_GET['action'])){ 
+		echo '<h3>Post '.$_GET['action'].'.</h3>'; 
+	} 
+	?>
+
+	<table>
+	<tr>
+		<th>Title</th>
+		<th>Date</th>
+		<th>Action</th>
+	</tr>
+
+	<?php
+		// Skapar en tabell för att hantera posterna (loopar) och visar de i ordning (med hjälp av query) 
+		// Varje inlägg har en redigera/radera länk som kallar på funktionen "delpost"
+		// Delpost baseras på postID. När man klickar på länken körs en confirm som frågar ifall man vill radera ett inlägg
+		try {
+
+			$stmt = $db->query('SELECT postID, postTitle, postDate FROM blog_posts ORDER BY postID DESC');
+			while($row = $stmt->fetch()){
+				
+				echo '<tr>';
+				echo '<td>'.$row['postTitle'].'</td>';
+				echo '<td>'.date('jS M Y', strtotime($row['postDate'])).'</td>';
+				?>
+
+				<td>
+					<a href="edit-post.php?id=<?php echo $row['postID'];?>">Redigera</a> | 
+					<a href="javascript:delpost('<?php echo $row['postID'];?>','<?php echo $row['postTitle'];?>')">Radera</a>
+				</td>
+				
+				<?php 
+				echo '</tr>';
+
+			}
+
+		} catch(PDOException $e) {
+		    echo $e->getMessage();
+		}
+	?>
+	</table>
+
+	<p><a href='skriv-post.php'>Lägg till</a></p>
+
+</div>
+
+</body>
+</html>
