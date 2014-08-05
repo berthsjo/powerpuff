86d0fdcc319a0a46e169d190c32f25b7b89f3b94
Graulf <raul.iribarren.c@gmail.com>
2014-08-05 16:20:20 +0200

    loggaut.php
    Lagt till logga ut sida.

---
+++ admin/loggaut.php
@@ -0,0 +1,9 @@
+<?php
+// Inkluderar config-filen 
+require_once('../includes/config.php');
+
+// Loggar ut användaren och omdirigerar till startsidan
+$user->logout();
+header('Location: index.php'); 
+
+?>
