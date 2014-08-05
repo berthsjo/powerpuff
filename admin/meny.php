db51aec3c8177c4d3d69b06a6b523778164bb38b
Erika Berthsjö <erika@berthsjo.com>
2014-08-05 11:39:14 +0200

    Meny
    Skapat meny till sidan

---
+++ admin/meny.php
@@ -0,0 +1,8 @@
+<img src="bilder/puff.png">
+<ul id='adminmeny'>
+  <li><a href="index.php">Blogg</a></li>
+  <li><a href="anvandare.php">Användare</a></li>
+  <li><a href="../" target="_blank">Kolla hemsidan</a></li>
+  <li><a href="logout.php">Logga ut</a></li>
+</ul>
+<div class="clear"></div>
