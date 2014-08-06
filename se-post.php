7d9454442798d7e05a54c2e4722b7e3e0fa81052
recksen <a_recksen@yahoo.se>
2014-08-06 15:14:44 +0200

    se-post.php
    Buggfix, undrar ärligt om jag kan ha varit full i går morse för vad fan
    hade jag gjort???

--- se-post.php
+++ se-post.php
@@ -1,4 +1,4 @@
-<!-- 
+<?php require('includes/config.php');
 
 //viewpost.php används för att visa inlägg som användaren klickat på.
 
@@ -8,48 +8,17 @@
 //köra queryn. När vi sedan kör $stmt->execute kommer datan i arryen skickas till databasens server.
 //Dessa två connectar aldrig vilket minimerar risken att någon fular i databasen.
 
-    $stmt = $db->prepare('SELECT postID, postTitle, postCont, postDate FROM blog_posts WHERE postID = :postID');
-    $stmt->execute(array(':postID' => $_GET['id']));
-    $row = $stmt->fetch();
-
-//Om det efterfrågade postID't inte existerar i databasen, skicka användaren tillbaka till index-sidan
-
-
-    if($row['postID'] == ''){
-    header('Location: ./');
-    exit;
-    }
-
-//Visa hela den valda posten.
-
-    echo '<div>';
-    echo '<h1>'.$row['postTitle'].'</h1>';
-    echo '<p>Posted on '.date('jS M Y', strtotime($row['postDate'])).'</p>';
-    echo '<p>'.$row['postCont'].'</p>';
-    echo '</div>';
-
- //Detta är enklast möjliga, förhoppningsvis är det enkelt att lägga till fler features.
-
-//viewpost.php används för att visa inlägg som klickade inlägg.
-
-//Queryn använder ett prepared statement. Infon som visas väljer man genom en $_GET[id] request som
-//skickar ett id.
-//Prepared statement funkar bättre än en vanlig query eftersom den förbereder databasen för att
-//köra queryn. När vi sedan kör $stmt->execute kommer datan i arryen skickas till databasens server.
-//Dessa två connectar aldrig vilket minimerar risken att någon fular i databasen. -->
-
-<?php require('includes/config.php');
-
 $stmt = $db->prepare('SELECT postID, postTitle, postCont, postDate FROM blog_posts WHERE postID = :postID');
 $stmt->execute(array(':postID' => $_GET['id']));
 $row = $stmt->fetch();
+
 //Om det efterfrågade postID't inte existerar i databasen, skicka användaren tillbaka till index-sidan
 
-if($row['postID'] == ''){
-    header('Location: ./');
-    exit;
-}
 
+if($row['postID'] == ''){
+  header('Location: ./');
+  exit;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -68,13 +37,13 @@ if($row['postID'] == ''){
         <p><a href="./">Blog Index</a></p>
 //Visa hela den valda posten.
 
-        <?php
-            echo '<div>';
-                echo '<h1>'.$row['postTitle'].'</h1>';
-                echo '<p>Posted on '.date('jS M Y', strtotime($row['postDate'])).'</p>';
-                echo '<p>'.$row['postCont'].'</p>';
-            echo '</div>';
-        ?>
+    <?php
+        echo '<div>';
+            echo '<h1>'.$row['postTitle'].'</h1>';
+            echo '<p>Posted on '.date('jS M Y', strtotime($row['postDate'])).'</p>';
+            echo '<p>'.$row['postCont'].'</p>';
+        echo '</div>';
+    ?>
 
     </div>
 
