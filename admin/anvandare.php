1b93c0201835aa154cb6a5d14db86a16d7660a02
recksen <a_recksen@yahoo.se>
2014-08-06 18:04:08 +0200

    gitignore och filen admin/använd
    Slängde en fil med bara bajs i samt ändrade en mening i anvädarfilen

--- admin/anvandare.php
+++ admin/anvandare.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // Inkluderar config-filen
 require_once('../includes/config.php');
 
@@ -8,7 +8,8 @@ if(!$user->is_logged_in()){header('location:loggain.php'); }
 // Visar meddelande från skriv-post / redigera-post sidan
 if(isset($_GET['deluser'])){
 
-  // Ignorerar om användarid är detsamma som 1, man ska inte kunna radera denna
+  // Garanterar minst en användare vars ID inte kan raderas övriga kan raderas.
+  //OBS! Vi har endast en anv. men vill ev. kunna lägga till fler
   if($_GET['deluser'] !='1'){
 
     $stmt = $db->prepare('DELETE FROM blog_members WHERE memberID = :memberID') ;
@@ -44,11 +45,11 @@ if(isset($_GET['deluser'])){
 
   <?php include('meny.php');?>
 
-  <?php 
+  <?php
   // Visar meddelande från skriv-post och redigera-post
-    if(isset($_GET['action'])){ 
-    echo '<h3>User '.$_GET['action'].'.</h3>'; 
-  } 
+    if(isset($_GET['action'])){
+    echo '<h3>User '.$_GET['action'].'.</h3>';
+  }
   ?>
 
     <table>
@@ -62,20 +63,20 @@ if(isset($_GET['deluser'])){
 
       $stmt = $db->query('SELECT memberID, username, email FROM blog_members ORDER BY username');
       while($row = $stmt->fetch()){
-        
+
         echo '<tr>';
         echo '<td>'.$row['username'].'</td>';
         echo '<td>'.$row['email'].'</td>';
         ?>
 
         <td>
-          <a href="edit-user.php?id=<?php echo $row['memberID'];?>">Edit</a> 
+          <a href="edit-user.php?id=<?php echo $row['memberID'];?>">Edit</a>
           <?php if($row['memberID'] != 1){?>
             | <a href="javascript:deluser('<?php echo $row['memberID'];?>','<?php echo $row['username'];?>')">Delete</a>
           <?php } ?>
         </td>
-        
-        <?php 
+
+        <?php
         echo '</tr>';
 
       }
