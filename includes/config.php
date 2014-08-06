d3707065c9dc3f868cc60bc15c121818f6eff47c
recksen <a_recksen@yahoo.se>
2014-08-06 20:41:14 +0200

    config
    rättar i kommentarerna

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 // ob = Output Buffering är en inbyggd funktion i PHP
-// Så länge den är aktiv skickas ingen data från scriptet (utom till headern). 
+// Så länge den är aktiv skickas ingen data från scriptet (utom till headern).
 // Datan sparas i intern buffert.
 
 ob_start();
@@ -27,19 +27,18 @@ function __autoload($class) {
   // strtolower = konverterar namn till små bokstäver (anv. namn) kollar om filen finns och inkluderar den.
   $class = strtolower($class);
 
-    //if call from within assets adjust the path !!! Kolla upp mer
+    //classpath är en autoload
    $classpath = 'klasser/klass.'.$class . '.php';
    if ( file_exists($classpath)) {
       require_once $classpath;
   }
 
-  //if call from within admin adjust the path !!! Kolla upp mer
    $classpath = '../klasser/klass.'.$class . '.php';
    if ( file_exists($classpath)) {
       require_once $classpath;
   }
 
-  //if call from within admin adjust the path !!! Kolla upp mer
+
    $classpath = '../../klasser/klass.'.$class . '.php';
    if ( file_exists($classpath)) {
       require_once $classpath;
