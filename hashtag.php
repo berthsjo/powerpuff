388e1dce2d43d8eb33a70d6c5583b4f2f73ae48b
Erika Berthsjö <erika@berthsjo.com>
2014-08-14 12:07:02 +0200

    Taggar fixade
    Hashtag fixat i hashtag.php

--- hashtag.php
+++ hashtag.php
@@ -1,8 +1,36 @@
 <?php
+require 'includes/config.php';
 
 if(isset($_GET['tag'])){
-  echo $_GET['tag'];
+  $tag = $_GET['tag'];
+
+try {
+
+        $stmt = $db->query("SELECT * FROM blog_posts WHERE LOCATE('$tag', postCont)");
+        while($row = $stmt->fetch()){
+
+          echo '<div>';
+            echo '<h1><a href="se-post.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
+            echo '<p>Upplagd '.date('j/N/Y H:i', strtotime($row['postDate'])).'</p>';
+            echo '<p>'.$row['postDesc'].'</p>';
+            echo '<p><a href="se-post.php?id='.$row['postID'].'">Läs mer</a></p>';
+          echo '</div>';
+          echo '<br/><br/>';
+
+        }
+
+
+
+      } catch(PDOException $e) {
+          echo $e->getMessage();
+      }
+
 }
+
+
+
+
+
 
 // if(isset($_GET["tag"])){
 //   $tag = preg_replace('#[^a-z0-9_]#i', '', $_GET["tag"]);
