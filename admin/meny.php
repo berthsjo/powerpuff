043904ef9c40fc34e5835f7f84178d4c32d98c8c
Erika Berthsjö <erika@berthsjo.com>
2014-08-16 21:59:36 +0200

    Responsivt
    Kika om ni tycker att de responsiva funkar och om de finns något kvar,
    SÅ FORT SOM MÖJLIGT. :)

--- admin/meny.php
+++ admin/meny.php
@@ -1,8 +1,8 @@
-<div id="logga"><a href="../index.php" target="_blank"><img src="../bilder/puff3.png"></a></div>
 <ul id='adminmeny'>
   <li><a href="index.php"><p class="menyn">Blogg</p></a></li>
   <li><a href="anvandare.php"><p class="menyn">Användare</p></a></li>
   <li><a href="../" target="_blank"><p class="menyn">Kolla hemsidan</p></a></li>
   <li><a href="loggaut.php"><p class="menyn">Logga ut</p></a></li>
 </ul>
+<div id="logga"><a href="../index.php" target="_blank"><img src="../bilder/puff3.png"></a></div>
 <div class="clear"></div>
