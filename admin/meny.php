ff2b5efd793edebc6c9b0c61b9835feabe76e32b
Erika Berthsjö <erika@berthsjo.com>
2014-08-16 22:06:01 +0200

    css fix
    lite mera fix

--- admin/meny.php
+++ admin/meny.php
@@ -2,7 +2,7 @@
   <li><a href="index.php"><p class="menyn">Blogg</p></a></li>
   <li><a href="anvandare.php"><p class="menyn">Användare</p></a></li>
   <li><a href="../" target="_blank"><p class="menyn">Kolla hemsidan</p></a></li>
-  <li><a href="loggaut.php"><p class="menyn">Logga ut</p></a></li>
+  <li><a href="loggaut.php"><p class="menyn">Logga ut</p></a></li><br>
+  <li><div id="logga"><a href="../index.php" target="_blank"><img src="../bilder/puff3.png"></a></div></li>
 </ul>
-<div id="logga"><a href="../index.php" target="_blank"><img src="../bilder/puff3.png"></a></div>
 <div class="clear"></div>
